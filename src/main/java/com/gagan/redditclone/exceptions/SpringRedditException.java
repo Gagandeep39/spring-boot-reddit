bda469c237613067b1bdb1b24b175428c91a3c43
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-12 23:01:32 +0530

    Created a custom exception

---
+++ src/main/java/com/gagan/redditclone/exceptions/SpringRedditException.java
@@ -0,0 +1,7 @@
+package com.gagan.redditclone.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+  public SpringRedditException(String message) {
+    super(message);
+  }
+}
