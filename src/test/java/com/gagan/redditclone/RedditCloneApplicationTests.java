f13cf5c9fcdea1d63b6d4643ee49d7788bb74e97
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-12 21:46:32 +0530

    Initial Commit

---
+++ src/test/java/com/gagan/redditclone/RedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gagan.redditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
