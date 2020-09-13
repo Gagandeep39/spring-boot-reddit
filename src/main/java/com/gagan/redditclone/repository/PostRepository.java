dc98ac45898b9e7c72fcc591661877d4c575a353
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-13 18:28:17 +0530

    Implemented some post related methods

--- src/main/java/com/gagan/redditclone/repository/PostRepository.java
+++ src/main/java/com/gagan/redditclone/repository/PostRepository.java
@@ -1,6 +1,11 @@
 package com.gagan.redditclone.repository;
 
+import java.util.List;
+
 import com.gagan.redditclone.model.Post;
+import com.gagan.redditclone.model.Subreddit;
+import com.gagan.redditclone.model.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +17,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+  List<Post> findAllBySubreddit(Subreddit subreddit);
+
+  List<Post> findByUser(User user);
 }
--- src/main/java/com/gagan/redditclone/repository/SubredditRepository.java
+++ src/main/java/com/gagan/redditclone/repository/SubredditRepository.java
@@ -1,5 +1,7 @@
 package com.gagan.redditclone.repository;
 
+import java.util.Optional;
+
 import com.gagan.redditclone.model.Subreddit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
+
+  Optional<Subreddit> findByName(String subredditName);
 }
